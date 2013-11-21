5d7ae7ce7e86a4f1fc867d7bf7add73a8e10a15a
Paul M. Jones <pmjones88@gmail.com>
2013-11-20 20:56:20 -0600

    use request/response as injections, not params

--- config/default/modify.php
+++ config/default/modify.php
@@ -2,16 +2,13 @@
 /**
  * @var $di The DI container.
  */
-$router = $di->get('web_router');
-
-$router->addValues(array(
-    'request' => $di->get('web_request'),
-    'response' => $di->get('web_response'),
-));
+$router   = $di->get('web_router');
+$request  = $di->get('web_request');
+$response = $di->get('web_response');
 
 $router->add('hello', '/')
     ->addValues(array(
-        'controller' => function ($request, $response) {
+        'controller' => function () use ($request, $response) {
             $response->content->set('Hello World!');
         }
     ));
