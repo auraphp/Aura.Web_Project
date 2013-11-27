d40cdbec97e2a1fccf58b8b45ecf97234defd2f5
Paul M. Jones <pmjones88@gmail.com>
2013-11-26 23:00:54 -0600

    move setup to kernel factory

--- web/index.php
+++ web/index.php
@@ -1,25 +1,20 @@
 <?php
-use Aura\Di\Config;
-use Aura\Di\Container;
-use Aura\Di\Forge;
-use Aura\Web_Kernel\WebKernel;
+use Aura\Web_Kernel\WebKernelFactory;
 
 // the project base directory
 $base = dirname(__DIR__);
 
-// autoloader
-$loader = require "{$base}/vendor/autoload.php";
-
-// DI container
-$di = new Container(new Forge(new Config));
-
-// config mode
+// the project config mode
 $file = str_replace("/", DIRECTORY_SEPARATOR, "{$base}/config/_mode");
 $mode = trim(file_get_contents($file));
 if (! $mode) {
     $mode = "default";
 }
 
+// autoloader
+$loader = require "{$base}/vendor/autoload.php";
+
 // create and invoke the project kernel
-$kernel = new WebKernel($loader, $di, $base, $mode);
+$factory = new WebKernelFactory;
+$kernel = $factory->newInstance($base, $mode, $loader);
 $kernel->__invoke();
