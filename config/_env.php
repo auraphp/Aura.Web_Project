ec4753a02c8a63e425001e99428dd3885e2f72f7
Paul M. Jones <pmjones88@gmail.com>
2013-12-18 15:20:53 -0600

    set mode only if not already set

--- config/_env.php
+++ config/_env.php
@@ -1,2 +1,6 @@
 <?php
-$_ENV['AURA_CONFIG_MODE'] = 'default';
+// set the mode here only if it is not already set.
+// this allows for setting via web server, integration testing, etc.
+if (! isset($_ENV['AURA_CONFIG_MODE'])) {
+    $_ENV['AURA_CONFIG_MODE'] = 'default';
+}
