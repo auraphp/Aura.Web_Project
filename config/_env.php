e87f19ba6bdd9a1e9d8d0de5efabc349f8fb2909
Paul M. Jones <pmjones88@gmail.com>
2013-12-19 17:01:25 -0600

    default mode is now 'dev' not 'default'

--- config/_env.php
+++ config/_env.php
@@ -2,5 +2,5 @@
 // set the mode here only if it is not already set.
 // this allows for setting via web server, integration testing, etc.
 if (! isset($_ENV['AURA_CONFIG_MODE'])) {
-    $_ENV['AURA_CONFIG_MODE'] = 'default';
+    $_ENV['AURA_CONFIG_MODE'] = 'dev';
 }
