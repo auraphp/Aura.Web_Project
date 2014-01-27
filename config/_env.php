9d5d3b82a9858e83426fb59db0c3629b8ad6aeef
Gareth Evans <garoevans@gmail.com>
2014-01-27 13:33:13 +0000

    Allow _env to check `getenv` as well as `$_ENV`

--- config/_env.php
+++ config/_env.php
@@ -2,5 +2,9 @@
 // set the mode here only if it is not already set.
 // this allows for setting via web server, integration testing, etc.
 if (! isset($_ENV['AURA_CONFIG_MODE'])) {
-    $_ENV['AURA_CONFIG_MODE'] = 'dev';
+    if (($aura_env = getenv('AURA_CONFIG_MODE')) !== false) {
+        $_ENV['AURA_CONFIG_MODE'] = $aura_env;
+    } else {
+        $_ENV['AURA_CONFIG_MODE'] = 'dev';
+    }
 }
