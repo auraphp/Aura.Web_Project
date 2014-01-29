3708d7f539c718d12a721a7bb215fc7de5b67833
Paul M. Jones <pmjones88@gmail.com>
2014-01-28 21:30:18 -0600

    stop using kernel script, embed boot logic in project

--- web/index.php
+++ web/index.php
@@ -8,6 +8,21 @@
  * @license http://opensource.org/licenses/bsd-license.php BSD
  * 
  */
+use Aura\Project_Kernel\ProjectContainer;
 
-// hand off to the kernel script
-require dirname(__DIR__) . '/vendor/aura/web-kernel/scripts/kernel.php';
+// the project base directory
+$base = dirname(__DIR__);
+
+// set up autoloader
+$loader = require "$base/vendor/autoload.php";
+$loader->add('', "{$base}/src");
+
+// load environment modifications
+require "{$base}/config/_env.php";
+
+// create the project container
+$di = ProjectContainer::factory($base, $loader, $_ENV, null);
+
+// create and invoke a web kernel
+$web_kernel = $di->newInstance('Aura\Web_Kernel\WebKernel');
+$web_kernel();
