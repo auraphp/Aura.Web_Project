631dbe5425dd6c3c3bd957f63bcea236acbc74a0
Paul M. Jones <pmjones88@gmail.com>
2014-04-02 16:11:07 -0500

    try to make this behave for travis

--- tests/WebProjectTest.php
+++ tests/WebProjectTest.php
@@ -6,23 +6,24 @@ class WebProjectTest extends \PHPUnit_Framework_TestCase
     public function testWeb()
     {
         $host = "127.0.0.1:8080";
-        $root = dirname(__DIR__) . '/web/';
-        $spec = array(
-            0 => array("pipe", "r"), // stdin
-            1 => array("pipe", "w"), // stdout
-            2 => array("pipe", "w")  // stderr
-        );
+        
+        // $root = dirname(__DIR__) . '/web/';
+        // $spec = array(
+        //     0 => array("pipe", "r"), // stdin
+        //     1 => array("pipe", "w"), // stdout
+        //     2 => array("pipe", "w")  // stderr
+        // );
 
-        $proc = proc_open(
-            "php -S {$host} -t {$root}",
-            $spec,
-            $pipes
-        );
+        // $proc = proc_open(
+        //     "php -S {$host} -t {$root}",
+        //     $spec,
+        //     $pipes
+        // );
 
         $actual = file_get_contents("http://{$host}/");
         $expect = 'Hello World!';
         $this->assertSame($expect, $actual);
 
-        proc_terminate($proc);
+        // proc_terminate($proc);
     }
 }
