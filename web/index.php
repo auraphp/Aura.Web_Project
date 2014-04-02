4dd4f633b5d944702ecc19941c2d01930f00c020
Paul M. Jones <pmjones88@gmail.com>
2014-04-02 13:35:35 -0500

    use newKernel()

--- web/index.php
+++ web/index.php
@@ -8,17 +8,10 @@
  * @license http://opensource.org/licenses/bsd-license.php BSD
  * 
  */
-
 $path = dirname(__DIR__);
 require "{$path}/vendor/autoload.php";
-require "{$path}/config/_env.php";
-
-$di = (new \Aura\Project_Kernel\Factory)->newContainer(
+$kernel = (new \Aura\Project_Kernel\Factory)->newKernel(
     $path,
-    $_ENV['AURA_CONFIG_MODE'],
-    "{$path}/composer.json",
-    "{$path}/vendor/composer/installed.json"
+    'Aura\Web_Kernel\WebKernel'
 );
-
-$kernel = $di->newInstance('Aura\Web_Kernel\WebKernel');
 $kernel();
